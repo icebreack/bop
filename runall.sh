298bc5a6530eabbc459b5fff5d68afd15f71fcef
Favarini <rodrigofavarini@gmail.com>
2016-11-09 15:59:44 -0200

    correção

--- runall.sh
+++ runall.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-sh bop_all.sh 5900 internet_vnc.txt;
-sh bop_all.sh 27017 internet_mongodb.txt
+sh bop.sh iprange_AWS.txt 27017 FinalAWS.txt
+sh bop.sh iprange_BR.txt 27017 FinalBR.txt
+
+sh bop.sh iprange_AWS.txt 5900 FinalAWS_VNC.txt
+sh bop.sh iprange_BR.txt 5900 FinalBR_VNC.txt
+sh bop.sh iprange_AZURE.txt 5900 FinalAZURE_VNC.txt
+
+#sh bop_all.sh 5900 internet_vnc.txt;
+#sh bop_all.sh 27017 internet_mongodb.txt
